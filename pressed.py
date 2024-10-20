1fb9775d6c04e4a3c9b09bb96fa535a10377033c
Scott Yeager <yeagersm@gmail.com>
2024-10-20 10:43:44 -0700

    Remove default actions

--- pressed.py
+++ pressed.py
@@ -1,8 +1,10 @@
 from threading import Thread, Timer
 
+
 class Button:
-    def __init__(self, hold_time=0, double_time=0, wait_hold=True,
-                name=None, number=None, **kwds):
+    def __init__(
+        self, hold_time=0, double_time=0, wait_hold=True, name=None, number=None, **kwds
+    ):
         self.hold_time = hold_time
         self.double_time = double_time
         self.wait_hold = wait_hold
@@ -22,10 +24,12 @@ class Button:
             self.double_timer = Thread()
 
     def __repr__(self):
-        return 'Button({}, {}, {}, {}, {})'.format(self.hold_time, self.double_time, self.name, self.number)
+        return "Button({}, {}, {}, {})".format(
+            self.hold_time, self.double_time, self.name, self.number
+        )
 
     def press(self):
-        if self.pressed: #Some devices send 'down' continually while pressed
+        if self.pressed:  # Some devices send 'down' continually while pressed
             return
 
         if not (self.hold_time or self.double_time):
@@ -50,8 +54,8 @@ class Button:
             return
 
         # Wait, doesn't this cause press action to trigger on both press and release in this case? Yeah, it does. Not sure why I thought this was a good idea...?
-        # if not (self.hold_time or self.double_time): 
-        #     self.press_action 
+        # if not (self.hold_time or self.double_time):
+        #     self.press_action
         #     self.pressed = False
         #     self.pressed_simultaneous = False
         #     return
@@ -69,7 +73,9 @@ class Button:
             self.double_timer = Timer(self.double_time, self.press_action)
             self.double_timer.start()
 
-        self.release_action(self) # Only useful when not using any of the fancy stuff, probably, but fire on every release for now
+        self.release_action(
+            self
+        )  # Only useful when not using any of the fancy stuff, probably, but fire on every release for now
 
         self.held = False
         self.pressed = False
@@ -83,13 +89,17 @@ class Button:
     # self as methods, while assigned functions are not methods and need
     # to be passed the button to reference stored state.
     def press_action(self, self2):
-        print('Pressed: ' + str(self))
+        pass
+        # print('Pressed: ' + str(self))
 
     def hold_action(self, self2):
-        print('Held: ' + str(self))
+        pass
+        # print('Held: ' + str(self))
 
     def double_action(self, self2):
-        print('Double pressed: ' + str(self))
+        pass
+        # print('Double pressed: ' + str(self))
 
     def release_action(self, self2):
-        print('Released: ' + str(self))
+        pass
+        # print('Released: ' + str(self))
